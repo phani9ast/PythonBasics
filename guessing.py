84aaea03249acfec65e6a3946b283158fa04489d
Phani Chilakapati <Phanidh.Chilakapati@indsci.com>
2025-07-31 15:46:09 -0400

    updated games for continuous run

--- guessing.py
+++ guessing.py
@@ -1,12 +1,26 @@
 import random
-num = 75
-while True:
-    user_input=int(input("Enter a number \n"))
-    if user_input < num:
-        print("Try a higher number")
-    elif user_input > num:
-        print("Try a lower number")
+num = random.randint(9,350)
+num_of_tries=0
+##is_exit=False
+print("type x to exit the game")
+while True: #and not is_exit:
+    if num_of_tries<5:
+        user_input=input("Enter a number \n")
+        if user_input == 'x':
+            break
+            #is_exit = True
+        elif int(user_input) < num:
+            print("Try a higher number")
+            num_of_tries+=1
+        elif int(user_input) > num:
+            print("Try a lower number")
+            num_of_tries+=1
+        else:
+            print("Bingo! You guessed the correct number ")
+            num_of_tries=0
+            num=random.randint(30,250)
     else:
-        print("Bingo! You guessed the correct number ")
-        num=random.randint(30,250)
-    
+        print("Too many tries, lets play again!")
+        print("type x to exit the game")
+        num=random.randint(9,350)
+        num_of_tries=0
